18e5d771d1ab6f513bafc47405a62207f3ed72b1
Alexander Casas <alexandercasasnqn@gmail.com>
2023-09-10 21:39:52 -0300

    +string patch fix

--- common/src/types/index.d.ts
+++ common/src/types/index.d.ts
@@ -21,7 +21,7 @@ export declare type TDefaultObject<T, K> = Partial<T> & K;
 declare global {
     interface Array<T> {
         /**
-         * @returns {number | null} The array length minus one. ```null``` if empty array.
+         * @returns {number | null} The array length minus one. ```null``` if empty array
          * @example
          * [1].lastIndex();
          * // returns 0;
@@ -34,15 +34,15 @@ declare global {
          */
         lastIndex(): number | null;
         /**
-         * @returns {T} An object with circular reference of items.
+         * @returns {T} An object with circular reference of items
          */
         isEmpty(): boolean;
         /**
-         * @returns {CircleElement<T> | null} An object with circular reference of items. ```null``` if empty array.
+         * @returns {CircleElement<T> | null} An object with circular reference of items. ```null``` if empty array
          */
         circle(): CircleElement<T> | null;
         /**
-         * @returns {T} A random element within the array.
+         * @returns {T} A random element within the array
          */
         random(): T;
         /**
@@ -54,15 +54,15 @@ declare global {
          * - If the index is greater than array's size, the function will start from the begining of the array.
          * - If the index is below than zero, the function will start from the end of the array.
          *
-         * @param {number} index - An index.
-         * @returns {T | undefined} The item on ```index``` position. ```undefined``` if empty array.
+         * @param {number} index - An index
+         * @returns {T | undefined} The item on ```index``` position. ```undefined``` if empty array
          */
         from(index: number): T | undefined;
         /**
          * Fills an array by a callback.
          *
-         * @param {function} filler - The callback that will be called to fill up the array.
-         * @returns {T[]} The filled array. Not a copy.
+         * @param {function} filler - The callback that will be called to fill up the array
+         * @returns {T[]} The filled array. Not a copy
          * @example
          *
          * [].fillWith((index, array) => ({continue: index < 49, value: index, push: true}));
@@ -78,9 +78,9 @@ declare global {
         /**
          * Rotates an array.
          *
-         * @param {T[]} this - The array to rotate.
-         * @param {RotateOptions} optionsOrSteps - The rotation options.
-         * @returns {T[]} A rotated copy of the array.
+         * @param {T[]} this - The array to rotate
+         * @param {RotateOptions} optionsOrSteps - The rotation options
+         * @returns {T[]} A rotated copy of the array
          * @example
          * const array = [1, 2, 3, 4, 5, 6, 7, 8, 9];
          * array.rotate();
@@ -105,10 +105,10 @@ declare global {
         /**
          * Split an array.
          *
-         * @param {T[]} this - The array to split.
-         * @param {number} steps - Depending by ```type``` parameter, the amount of sections to get.
-         * @param {SplitType} type - The split type.
-         * @returns {T[][]} An array filled with the divided array.
+         * @param {T[]} this - The array to split
+         * @param {number} steps - Depending by ```type``` parameter, the amount of sections to get
+         * @param {SplitType} type - The split type
+         * @returns {T[][]} An array filled with the divided array
          * @example
          * const array = [1, 2, 3, 4, 5, 6, 7, 8, 9];
          * array.split(1);
@@ -126,9 +126,9 @@ declare global {
         split<T>(this: T[], steps: number, type?: SplitType): T[][];
         /**
          *
-         * @param {T[]} this - The array which elements will be discriminated.
-         * @param {function} filter - Function that will filter the items.
-         * @returns {DiscriminateResult<T>} An object with "true" and "false" members that are filled with the array items that passed the filter or not.
+         * @param {T[]} this - The array which elements will be discriminated
+         * @param {function} filter - Function that will filter the items
+         * @returns {DiscriminateResult<T>} An object with "true" and "false" members that are filled with the array items that passed the filter or not
          * @example
          * const array = [1, 2, 3, 4, 5, 6, 7, 8, 9];
          *
@@ -149,8 +149,8 @@ declare global {
          *
          * Copies an object deeply.
          *
-         * @param {object} object - The object that will be copied.
-         * @returns {object} A copy of the first object.
+         * @param {object} object - The object that will be copied
+         * @returns {object} A copy of the first object
          */
         clone<T extends object>(object: T): T;
         /**
@@ -159,10 +159,10 @@ declare global {
          * Update the first object with the values of the second object.
          * The changes just happens over the existing fields.
          *
-         * @param {object} firstObject - The object that will be updated.
-         * @param {object} secondObject - The object to use for the update of the firstObject.
-         * @param {function} [filter] - A function that help to choose which elements will be updated.
-         * @returns {object} An updated copy of the first object.
+         * @param {object} firstObject - The object that will be updated
+         * @param {object} secondObject - The object to use for the update of the firstObject
+         * @param {function} [filter] - A function that help to choose which elements will be updated
+         * @returns {object} An updated copy of the first object
          */
         patch<T extends object>(firstObject: T, secondObject: DeepPartial<T>, filter?: (options: PatchFilterOptions) => boolean): T;
         /**
@@ -170,7 +170,7 @@ declare global {
          *
          * Creates an array from an object.
          * @param {object} object - The source object
-         * @returns {Indexable<object>} A new array with the fields of the object.
+         * @returns {Indexable<object>} A new array with the fields of the object
          */
         toArray<T extends object>(object: T): Indexable<T>;
         /**
@@ -180,7 +180,7 @@ declare global {
          *
          * @param {object} object - The source object
          * @param {string[]} paths - The property paths to pick
-         * @returns {Pick<object, string[]>} A new object with the choosen fields.
+         * @returns {Pick<object, string[]>} A new object with the choosen fields
          * @see https://gist.github.com/bisubus/2da8af7e801ffd813fab7ac221aa7afc?permalink_comment_id=2366292#gistcomment-2366292
          */
         pick<T extends object, K extends keyof T>(object: T, paths: K[], deepClone?: boolean): Pick<T, K>;
@@ -192,6 +192,22 @@ declare global {
         }): number;
     }
     interface String {
+        /**
+         * @since 1.1.16
+         *
+         * @param {string} this - The string to patch
+         * @param {object} object - The object to use for the patch
+         * @returns {string} A patched copy of the string.
+         * @example
+         * "Hello name".patch({name: "John"});
+         * // Returns "Hello John"
+         *
+         * "Hello name".patch({name: "John", surname: "Doe"});
+         * // Returns "Hello John"
+         *
+         * "My name is MY_nAME and i'm YEARS_OlD years old".patch({"/MY_NAME/i": "John", "/YEARS_OlD/i": 30});
+         * // Returns "My name is John and i'm 30 years old"
+         */
         patch(this: String, object: object): String;
     }
 }
